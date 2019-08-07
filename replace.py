5df08aa6ad65fdb3348611a0a59c5615d31e4a14
Alex <ovechko.056@gmail.com>
2019-08-07 14:32:35 +0300

    Delete unused files.

--- replace.py
+++
@@ -1,8 +0,0 @@
-file = open('trainer/2012_train.txt', 'r+')
-content = file.read()
-content = content.replace(r'C:/Users/Alexandr/IdeaProjects/keras-yolo3', 'gs://some_test_bucket-12121212')
-
-file.seek(0)
-file.write(content)
-file.truncate()
-file.close()
